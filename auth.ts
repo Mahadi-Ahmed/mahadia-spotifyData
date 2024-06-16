2b9c5cf667ad1b80c637aa083b718209e58a88a9
Mahadi-Ahmed <mahadia.dev@gmail.com>
2024-06-16 22:56:30 +0200

    add request to get access token
    use client credentials flow to get a access token

---
+++ auth.ts
@@ -0,0 +1,27 @@
+// Client credentials flow
+
+export const getBearerToken = async () => {
+  const client = process.env.SPOTIFY_CLIENT
+  const secret = process.env.SPOTIFY_SECRET
+  const authOptions = {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Basic ' + Buffer.from(client + ':' + secret).toString('base64'),
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      grant_type: 'client_credentials'
+    })
+  };
+
+  try {
+    const response = await fetch('https://accounts.spotify.com/api/token', authOptions)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+getBearerToken()
+
