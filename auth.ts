493955fc3e9f19ef3cd25d0d8040ed8dd2694022
Mahadi-Ahmed <mahadia.dev@gmail.com>
2024-06-17 00:00:10 +0200

    add type to token request

--- auth.ts
+++ auth.ts
@@ -1,6 +1,12 @@
 // Client credentials flow
 
-export const getBearerToken = async () => {
+type AuthTokenResponse = {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+};
+
+export const getBearerToken = async (): Promise<AuthTokenResponse> => {
   const client = process.env.SPOTIFY_CLIENT
   const secret = process.env.SPOTIFY_SECRET
   const authOptions = {
@@ -16,9 +22,10 @@ export const getBearerToken = async () => {
 
   try {
     const response = await fetch('https://accounts.spotify.com/api/token', authOptions)
-    const data = await response.json()
+    const data = await response.json() as AuthTokenResponse
     return data
   } catch (error) {
     console.log(error)
+    throw new Error(`${error}`)
   }
 }
