a648eb0fb429d7c80bc5173200c5a5998aeb50e4
Mahadi-Ahmed <mahadia.dev@gmail.com>
2024-06-17 21:29:25 +0200

    get count of all data sheets in folder

--- goSpotify/main.go
+++ goSpotify/main.go
@@ -9,30 +9,34 @@ import (
 )
 
 func main() {
-	endsong_0, err := os.Open("../rawSpotifyData/endsong_0.json")
+	fileIndex := 9
 
-	if err != nil {
-		fmt.Println(err)
+  totalCount := 0
+	for i := 0; i <= fileIndex; i++ {
+		fileName := fmt.Sprintf("%s%d%s", "../rawSpotifyData/MyDataGo/endsong_", i, ".json")
+		jsonFile, err := os.Open(fileName)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer jsonFile.Close()
+
+		// Read the JSON file content
+		byteValue, err := io.ReadAll(jsonFile)
+		var spotifyData []models.SpotifyData
+		err = json.Unmarshal(byteValue, &spotifyData)
+
+		if err != nil {
+			fmt.Println("kaos with unmarshal spotify data", err)
+		}
+		totalCount += getObjectCount(spotifyData)
 	}
-	defer endsong_0.Close()
-
-	// Read the JSON file content
-	byteValue, err := io.ReadAll(endsong_0)
-	var spotifyData []models.SpotifyData
-	err = json.Unmarshal(byteValue, &spotifyData)
-	if err != nil {
-		fmt.Println("kaos with unmarshal spotify data", err)
-	}
-  var d0 = getObjectCount(spotifyData)
-  fmt.Println(d0)
-
+  fmt.Println(totalCount)
 }
 
-
 func getObjectCount(dataSheet []models.SpotifyData) (count int) {
-  count = 0
-  for i:= range dataSheet {
-    count = i
-  }
-  return count + 1
+	count = 0
+	for i := range dataSheet {
+		count = i
+	}
+	return count + 1
 }
