1a4e966df2f23f8af1c3cccd3f37259735301bb6
Mahadi-Ahmed <mahadia.dev@gmail.com>
2024-06-17 14:24:02 +0200

    try out a cople of spotifys endpoint

--- main.ts
+++ main.ts
@@ -1,1 +1,49 @@
-console.log("Hello via Bun!");
+import { getBearerToken } from "./auth";
+
+const { access_token } = await getBearerToken()
+const headers = {
+  'Authorization': `Bearer ${access_token}`,
+}
+
+const search = async () => {
+  const q = 'track 3clX2NMmjaAHmBjeTSa9vV'
+  const type = 'track'
+  const url = `${process.env.SPOTIFY_API_URL}/search?q=${q}&type=${type}&market=sv`
+
+  try {
+    const response = await fetch(url, { headers })
+    const data = await response.json()
+    console.log(JSON.stringify(data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const track = async (trackId: string) => {
+  const url = `${process.env.SPOTIFY_API_URL}/tracks/${trackId}?market=sv`
+
+  try {
+    const response = await fetch(url, { headers })
+    const data = await response.json()
+    console.log(JSON.stringify(data))
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+
+const getArtist = async (artistId: string) => {
+  const url = `${process.env.SPOTIFY_API_URL}/artists/${artistId}`
+
+  try {
+    const response = await fetch(url, { headers })
+    const data = await response.json()
+    console.log(JSON.stringify(data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// track('3clX2NMmjaAHmBjeTSa9vV')
+getArtist('7BMccF0hQFBpP6417k1OtQ')
+// search()
