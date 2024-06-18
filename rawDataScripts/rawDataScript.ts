e3d4b67c7f7bf998f728c885d9ae4909a050f051
Mahadi-Ahmed <mahadia.dev@gmail.com>
2024-06-18 20:17:03 +0200

    use json instead of ts when importing data sheets

--- rawDataScripts/rawDataScript.ts
+++ rawDataScripts/rawDataScript.ts
@@ -1,17 +1,18 @@
 // import { data } from '../rawSpotifyData/smallSample.ts'
 import type { PlaybackData } from "./types";
-import { endsong_0 } from '../rawSpotifyData/MyDataAsTs/endsong_0.ts'
-import { endsong_1 } from '../rawSpotifyData/MyDataAsTs/endsong_1.ts'
-import { endsong_2 } from '../rawSpotifyData/MyDataAsTs/endsong_2.ts'
-import { endsong_3 } from '../rawSpotifyData/MyDataAsTs/endsong_3.ts'
-import { endsong_4 } from '../rawSpotifyData/MyDataAsTs/endsong_4.ts'
-import { endsong_5 } from '../rawSpotifyData/MyDataAsTs/endsong_5.ts'
-import { endsong_6 } from '../rawSpotifyData/MyDataAsTs/endsong_6.ts'
-import { endsong_7 } from '../rawSpotifyData/MyDataAsTs/endsong_7.ts'
-import { endsong_8 } from '../rawSpotifyData/MyDataAsTs/endsong_8.ts'
-import { endsong_9 } from '../rawSpotifyData/MyDataAsTs/endsong_9.ts'
+import endsong_0 from '../rawSpotifyData/MyData/endsong_0.json'
+import endsong_1 from '../rawSpotifyData/MyData/endsong_1.json'
+import endsong_2 from '../rawSpotifyData/MyData/endsong_2.json'
+import endsong_3 from '../rawSpotifyData/MyData/endsong_3.json'
+import endsong_4 from '../rawSpotifyData/MyData/endsong_4.json'
+import endsong_5 from '../rawSpotifyData/MyData/endsong_5.json'
+import endsong_6 from '../rawSpotifyData/MyData/endsong_6.json'
+import endsong_7 from '../rawSpotifyData/MyData/endsong_7.json'
+import endsong_8 from '../rawSpotifyData/MyData/endsong_8.json'
+import endsong_9 from '../rawSpotifyData/MyData/endsong_9.json'
+import data from '../rawSpotifyData/smallSample.json'
 
-// const playbackData: PlaybackData[] = data
+const playbackData: PlaybackData[] = data
 const playbackData0: PlaybackData[] = endsong_0
 const playbackData1: PlaybackData[] = endsong_1
 const playbackData2: PlaybackData[] = endsong_2
@@ -23,6 +24,7 @@ const playbackData7: PlaybackData[] = endsong_7
 const playbackData8: PlaybackData[] = endsong_8
 const playbackData9: PlaybackData[] = endsong_9
 
+const endsong =  playbackData.length
 const endsong0Entries = playbackData0.length
 const endsong1Entries = playbackData1.length
 const endsong2Entries = playbackData2.length
