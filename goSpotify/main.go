1a083cad570957b341a48bf248c16ae0b36cb7a6
Mahadi-Ahmed <mahadia.dev@gmail.com>
2024-06-19 23:53:51 +0200

    refactor: use pool to connect to db

--- goSpotify/main.go
+++ goSpotify/main.go
@@ -1,28 +1,45 @@
 package main
 
 import (
-	// "fmt"
 	"context"
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 )
 
 type postgres struct {
-	db *pgx.Conn
+	db *pgxpool.Pool
 }
 
-func NewPG(ctx context.Context, connStr string) (*pgx.Conn, error) {
-	// NOTE: pgx.Connect is not concurrency safe, if needed look into pgxpool@github.com/jackc/pgx/v5/pgxpool
-	conn, err := pgx.Connect(context.Background(), connStr)
-	if err != nil {
-		fmt.Println("unable to connect to database", err)
-		os.Exit(1)
+var (
+  pgInstance *postgres
+  pgOnce sync.Once
+)
+
+func NewPG(ctx context.Context, connStr string) (*postgres, error) {
+	var initErr error
+	pgOnce.Do(func() {
+		db, err := pgxpool.New(ctx, connStr)
+		if err != nil {
+			initErr = fmt.Errorf("unable to create connection pool: %w", err)
+			return
+		}
+		pgInstance = &postgres{db}
+	})
+
+	if initErr != nil {
+		return nil, initErr
 	}
-  return conn, nil
+
+	return pgInstance, nil
+}
+
+func (pg *postgres) Close() {
+	pg.db.Close()
 }
 
 func init() {
@@ -41,11 +58,11 @@ func main() {
 	)
 
 	connStr := "postgres://" + dbUser + ":" + dbPassword + "@" + dbHost + "/" + dbName + "?sslmode=disable"
-  db, err := NewPG(context.Background(), connStr)
-	defer db.Close(context.Background())
+  pool, err := NewPG(context.Background(), connStr)
+  defer pool.Close()
 
 	var employees string
-	err = db.QueryRow(context.Background(), "select name from employees where salary=65000").Scan(&employees)
+	err = pool.db.QueryRow(context.Background(), "select name from employees where salary=65000").Scan(&employees)
 	if err != nil {
 		fmt.Println("Query failed")
 		fmt.Println(err)
