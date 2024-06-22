1926d105a4f032d535c9f64e3702215abcea998f
Mahadi-Ahmed <mahadia.dev@gmail.com>
2024-06-22 16:15:52 +0200

    add structs for insertions

--- goSpotify/models/spotify_data.go
+++ goSpotify/models/spotify_data.go
@@ -4,9 +4,42 @@ import (
 	"time"
 )
 
+type Users struct {
+	UserName string `json:"username"`
+}
+
+type Track struct {
+	TrackID         *string
+	TrackName       string  `json:"master_metadata_track_name"`
+	ArtistName      string  `json:"master_metadata_album_artist_name"`
+	AlbumAlbumName  string  `json:"master_metadata_album_album_name"`
+	SpotifyTrackUri string  `json:"spotify_track_uri"`
+	EpisodeName     *string `json:"episode_name"`
+	EpisodeShowName *string `json:"episode_show_name"`
+}
+
+type Playback struct {
+	Id                 string
+	UserName           string
+	TrackId            string
+	Timestamp          time.Time `json:"ts"`
+	Platform           string    `json:"platform"`
+	MsPlayed           int       `json:"ms_played"`
+	ConnCountry        string    `json:"conn_country"`
+	IpAddrDecrypted    string    `json:"ip_addr_decrypted"`
+	UserAgentDecrypted string    `json:"user_agent_decrypted"`
+	ReasonStart        string    `json:"reason_start"`
+	ReasonEnd          string    `json:"reason_end"`
+	Shuffle            bool      `json:"shuffle"`
+	Skipped            bool      `json:"skipped"`
+	Offline            bool      `json:"offline"`
+	OfflineTimestamp   int64     `json:"offline_timestamp"`
+	IncognitoMode      bool      `json:"incognito_mode"`
+}
+
 type SpotifyData struct {
 	Timestamp                     time.Time `json:"ts"`
-	Username                      string    `json:"username"`
+	UserName                      string    `json:"username"`
 	Platform                      string    `json:"platform"`
 	MsPlayed                      int       `json:"ms_played"`
 	ConnCountry                   string    `json:"conn_country"`
