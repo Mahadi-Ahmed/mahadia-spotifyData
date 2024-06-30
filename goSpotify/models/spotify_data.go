522825e72806c3247779fe1c7885a2be6efeb338
Mahadi-Ahmed <mahadia.dev@gmail.com>
2024-06-30 17:02:32 +0200

    update model

--- goSpotify/models/spotify_data.go
+++ goSpotify/models/spotify_data.go
@@ -8,15 +8,19 @@ type Users struct {
 	UserName *string
 }
 
+type Podcast struct {
+	PodcastId         string
+	EpisodeName       string
+	EpisodeShowName   string
+	SpotifyEpisodeUri string
+}
+
 type Track struct {
-	TrackID           *string
-	TrackName         *string
-	ArtistName        *string
-	AlbumName         *string
-	SpotifyTrackUri   *string
-	EpisodeName       *string
-	EpisodeShowName   *string
-	SpotifyEpisodeUri *string
+	TrackID         string
+	TrackName       string
+	ArtistName      string
+	AlbumName       string
+	SpotifyTrackUri string
 }
 
 type Playback struct {
@@ -36,8 +40,10 @@ type Playback struct {
 	Offline            bool      `json:"offline"`
 	OfflineTimestamp   int64     `json:"offline_timestamp"`
 	IncognitoMode      bool      `json:"incognito_mode"`
+	PodcastId          string
 }
 
+// NOTE: use pointers on possible null values
 type SpotifyData struct {
 	Timestamp                     time.Time `json:"ts"`
 	UserName                      string    `json:"username"`
@@ -46,13 +52,13 @@ type SpotifyData struct {
 	ConnCountry                   string    `json:"conn_country"`
 	IpAddrDecrypted               string    `json:"ip_addr_decrypted"`
 	UserAgentDecrypted            string    `json:"user_agent_decrypted"`
-	MasterMetadataTrackName       string    `json:"master_metadata_track_name"`
-	MasterMetadataAlbumArtistName string    `json:"master_metadata_album_artist_name"`
-	MasterMetadataAlbumAlbumName  string    `json:"master_metadata_album_album_name"`
-	SpotifyTrackUri               string    `json:"spotify_track_uri"`
-  EpisodeName                   *string   `json:"episode_name"` // NOTE: use pointers on possible null values
-	EpisodeShowName               *string   `json:"episode_show_name"`
-	SpotifyEpisodeUri             *string   `json:"spotify_episode_uri"`
+	MasterMetadataTrackName       *string    `json:"master_metadata_track_name"`
+	MasterMetadataAlbumArtistName *string    `json:"master_metadata_album_artist_name"`
+	MasterMetadataAlbumAlbumName  *string    `json:"master_metadata_album_album_name"`
+	SpotifyTrackUri               *string    `json:"spotify_track_uri"`
+	EpisodeName                   *string    `json:"episode_name"` 
+	EpisodeShowName               *string    `json:"episode_show_name"`
+	SpotifyEpisodeUri             *string    `json:"spotify_episode_uri"`
 	ReasonStart                   string    `json:"reason_start"`
 	ReasonEnd                     string    `json:"reason_end"`
 	Shuffle                       bool      `json:"shuffle"`
