62e589c6a7eb45b22e5e2f099ef64982e1388bf7
Mahadi-Ahmed <mahadia.dev@gmail.com>
2024-07-07 02:02:43 +0200

    add test for insertIntoDb

--- goSpotify/pg/pg_test.go
+++ goSpotify/pg/pg_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/mahadia/mahadia-spotifyData/goSpotify/pg/testHelpers"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"github.com/testcontainers/testcontainers-go"
@@ -20,17 +21,46 @@ type PostgresTestSuite struct {
 	pg        *Postgres
 }
 
-
 func (suite *PostgresTestSuite) TestDropAllTable() {
-  t := suite.T()
-  err := suite.pg.DropAllTables(suite.ctx)
-  assert.NoError(t, err , "Failed to create all tables")
+	t := suite.T()
+	err := suite.pg.DropAllTables(suite.ctx)
+	assert.NoError(t, err, "Failed to create all tables")
 }
 
 func (suite *PostgresTestSuite) TestCreateAllTable() {
-  t := suite.T()
-  err := suite.pg.CreateAllTables(suite.ctx)
-  assert.NoError(t, err , "Failed to create all tables")
+	t := suite.T()
+	err := suite.pg.CreateAllTables(suite.ctx)
+	assert.NoError(t, err, "Failed to create all tables")
+}
+
+func (suite *PostgresTestSuite) TestInsertTrackIntoDbTable() {
+	t := suite.T()
+	suite.pg.CreateAllTables(suite.ctx)
+	err := suite.pg.InsertIntoDb(suite.ctx, pg_testhelper.TestDataValidTrack)
+	assert.NoError(t, err, "Failed funciton InsertIntoDb")
+
+	var countPlayback int
+	var countTrack int
+	var countPodcast int
+	var countUser int
+	var countMedia int
+
+	errCountPlayback := suite.pg.Db.QueryRow(suite.ctx, "select count(*) from playback").Scan(&countPlayback)
+	errCountTrack := suite.pg.Db.QueryRow(suite.ctx, "select count(*) from track").Scan(&countTrack)
+	errCountPodcast := suite.pg.Db.QueryRow(suite.ctx, "select count(*) from podcast").Scan(&countPodcast)
+	errCountUser := suite.pg.Db.QueryRow(suite.ctx, "select count(*) from user").Scan(&countUser)
+	errCountMedia := suite.pg.Db.QueryRow(suite.ctx, "select count(*) from media").Scan(&countMedia)
+	assert.NoError(t, errCountPlayback, "Failed to query playback")
+	assert.NoError(t, errCountTrack, "Failed to query track")
+	assert.NoError(t, errCountPodcast, "Failed to query podcast")
+	assert.NoError(t, errCountUser, "Failed to query user")
+	assert.NoError(t, errCountMedia, "Failed to query media")
+
+	assert.Equal(t, 1, countPlayback, "playback was not inserted correctly")
+	assert.Equal(t, 1, countTrack, "track was not inserted correctly")
+	assert.Equal(t, 0, countPodcast, "podcast was not inserted correctly")
+	assert.Equal(t, 1, countUser, "user was not inserted correctly")
+	assert.Equal(t, 1, countMedia, "media was not inserted correctly")
 }
 
 func (suite *PostgresTestSuite) SetupSuite() {
